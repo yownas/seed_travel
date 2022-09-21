d607cd1acad80d0b02f0cb7beed6a277eb848962
Justin Maier <Zipp425@gmail.com>
2022-09-20 19:17:53 -0600

    Save to folder

--- scripts/seed_travel.py
+++ scripts/seed_travel.py
@@ -1,3 +1,4 @@
+import os
 import modules.scripts as scripts
 import gradio as gr
 import math
@@ -15,17 +16,44 @@ class Script(scripts.Script):
     def ui(self, is_img2img):
         unsinify = gr.Checkbox(label='Reduce effect of sin() during interpolation', value=True)
         dest_seed = gr.Textbox(label="Destination seed(s) (Comma separated)", lines=1)
-        steps = gr.Textbox(label="Steps", lines=1)
+        steps = gr.Number(label="Steps", value=10)
 
         return [dest_seed, steps, unsinify]
+
+    def get_next_sequence_number(path):
+        from pathlib import Path
+        """
+        Determines and returns the next sequence number to use when saving an image in the specified directory.
+
+        The sequence starts at 0.
+        """
+        result = -1
+        dir = Path(path)
+        for file in dir.iterdir():
+            if not file.is_dir(): continue
+            try:
+                num = int(file.name)
+                if num > result: result = num
+            except ValueError:
+                pass
+        return result + 1
 
     def run(self, p, dest_seed, steps, unsinify):
         initial_info = None
         images = []
 
+        # Custom seed travel saving
+        travel_path = os.path.join(p.outpath_samples, "travels")
+        os.makedirs(travel_path, exist_ok=True)
+        travel_number = Script.get_next_sequence_number(travel_path)
+        travel_path = os.path.join(travel_path, f"{travel_number:05}")
+        p.outpath_samples = travel_path
+
         start_seed = p.seed
         seeds = [int(x.strip()) for x in dest_seed.split(",")]
-        print(f"Generating {((int(steps)) * len(seeds)) + 1} images.")
+        total_images = int(steps) * len(seeds) + 1
+        print(f"Generating {total_images} images.")
+        state.job_count = total_images # Set the job count to the total number of images to be generated
         for next_seed in seeds:
             p.seed = start_seed
             p.subseed = next_seed
