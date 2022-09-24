802b7ad412713c12bea7c1e3d972f8d2a12ab88a
Jonas Stare <yownas@gmail.com>
2022-09-24 14:49:57 +0200

    Attempt to declutter some of the settings

--- scripts/seed_travel.py
+++ scripts/seed_travel.py
@@ -15,16 +15,32 @@ class Script(scripts.Script):
         return True
 
     def ui(self, is_img2img):
-        unsinify = gr.Checkbox(label='Reduce effect of sin() during interpolation', value=True)
-        rnd_seed = gr.Checkbox(label='Use Random seeds', value=False)
-        seed_count = gr.Number(label="Number of random seed(s) (Only used if random seeds is checked)", value=4)
-        dest_seed = gr.Textbox(label="Destination seed(s) (Comma separated)", lines=1)
-        steps = gr.Number(label="Steps", value=10)
-        loopback = gr.Checkbox(label='Loop back to initial seed', value=True)
-        save_video = gr.Checkbox(label='Save results as video', value=True)
-        video_fps = gr.Number(label='Frames per second', value=30)
+        def gr_show(visible=True):
+            return {"visible": visible, "__type__": "update"}
 
-        return [rnd_seed, seed_count, dest_seed, steps, unsinify, loopback, save_video, video_fps]
+        def change_visibility(show):
+            return {comp: gr_show(show) for comp in seed_travel_extra}
+
+        seed_travel_extra = []
+
+        dest_seed = gr.Textbox(label='Destination seed(s) (Comma separated)', lines=1)
+        steps = gr.Number(label='Steps', value=10)
+        seed_travel_toggle_extra = gr.Checkbox(label='Show extra settings', value=False)
+
+        with gr.Row(visible=False) as seed_travel_extra_row:
+            seed_travel_extra.append(seed_travel_extra_row)
+            with gr.Box() as seed_travel_box1:
+                rnd_seed = gr.Checkbox(label='Only use Random seeds', value=False)
+                seed_count = gr.Number(label='Number of random seed(s)', value=4)
+                unsinify = gr.Checkbox(label='Reduce effect of sin() during interpolation', value=True)
+            with gr.Box() as seed_travel_box2:
+                loopback = gr.Checkbox(label='Loop back to initial seed', value=False)
+                save_video = gr.Checkbox(label='Save results as video', value=True)
+                video_fps = gr.Number(label='Frames per second', value=30)
+
+        seed_travel_toggle_extra.change(change_visibility, show_progress=False, inputs=[seed_travel_toggle_extra], outputs=seed_travel_extra)        
+
+        return [rnd_seed, seed_count, dest_seed, steps, unsinify, loopback, save_video, video_fps, seed_travel_toggle_extra]
 
     def get_next_sequence_number(path):
         from pathlib import Path
@@ -43,7 +59,7 @@ class Script(scripts.Script):
                 pass
         return result + 1
 
-    def run(self, p, rnd_seed, seed_count, dest_seed, steps, unsinify, loopback, save_video, video_fps):
+    def run(self, p, rnd_seed, seed_count, dest_seed, steps, unsinify, loopback, save_video, video_fps, seed_travel_toggle):
         initial_info = None
         images = []
 
