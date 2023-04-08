972cc439225ae6993b8c43bf4b677c9f16e3ead1
Jonas Stare <yownas@gmail.com>
2023-04-08 17:12:23 +0200

    Make matplotlib thread-safe(?)

--- scripts/seed_travel.py
+++ scripts/seed_travel.py
@@ -1,6 +1,8 @@
 import gradio as gr
 import math
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 import numpy as np
 import os
 from PIL import Image
