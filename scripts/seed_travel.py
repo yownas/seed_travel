1a97ebb43428b43c41001cfe79e50b468126d99f
Jonas Stare <yownas@gmail.com>
2023-04-19 19:54:13 +0200

    Added better error handling when making video

--- scripts/seed_travel.py
+++ scripts/seed_travel.py
@@ -104,6 +104,7 @@ class Script(scripts.Script):
             tgt_w, tgt_h = round(p.width * upscale_ratio), round(p.height * upscale_ratio)
         else:
             tgt_w, tgt_h = p.width, p.height
+        video_fps = 0 if video_fps == None else video_fps
         save_video = video_fps != 0
 
         def st_fixseed(seed):
@@ -401,11 +402,14 @@ class Script(scripts.Script):
 
             # Save video before continuing with SSIM-stats and RIFE (If things crashes we will atleast have this video)
             if save_video:
-                frames = [np.asarray(step_images[0])] * lead_inout + [np.asarray(t) for t in step_images] + [np.asarray(step_images[-1])] * lead_inout
-                fps = video_fps if video_fps > 0 else len(frames) / abs(video_fps)
-                clip = ImageSequenceClip.ImageSequenceClip(frames, fps=fps)
-                filename = f"travel-{travel_number:05}-{s:04}.mp4" if compare_paths else f"travel-{travel_number:05}.mp4"
-                clip.write_videofile(os.path.join(travel_path, filename), verbose=False, logger=None)
+                try:
+                    frames = [np.asarray(step_images[0])] * lead_inout + [np.asarray(t) for t in step_images] + [np.asarray(step_images[-1])] * lead_inout
+                    fps = video_fps if video_fps > 0 else len(frames) / abs(video_fps)
+                    clip = ImageSequenceClip.ImageSequenceClip(frames, fps=fps)
+                    filename = f"travel-{travel_number:05}-{s:04}.mp4" if compare_paths else f"travel-{travel_number:05}.mp4"
+                    clip.write_videofile(os.path.join(travel_path, filename), verbose=False, logger=None)
+                except:
+                    print(f"ERROR: Failed generating video")
 
             # SSIM-stats
             if save_stats and ssim_diff > 0:
@@ -557,11 +561,14 @@ class Script(scripts.Script):
                     #    buffer.put(frame)
                     rife_images = buffer
     
-                frames = [np.asarray(rife_images[0])] * lead_inout + [np.asarray(t) for t in rife_images] + [np.asarray(rife_images[-1])] * lead_inout
-                fps = video_fps if video_fps > 0 else len(frames) / abs(video_fps)
-                clip = ImageSequenceClip.ImageSequenceClip(frames, fps=fps)
-                filename = f"travel-rife-{travel_number:05}.mp4"
-                clip.write_videofile(os.path.join(travel_path, filename), verbose=False, logger=None)
+                try:
+                    frames = [np.asarray(rife_images[0])] * lead_inout + [np.asarray(t) for t in rife_images] + [np.asarray(rife_images[-1])] * lead_inout
+                    fps = video_fps if video_fps > 0 else len(frames) / abs(video_fps)
+                    clip = ImageSequenceClip.ImageSequenceClip(frames, fps=fps)
+                    filename = f"travel-rife-{travel_number:05}.mp4"
+                    clip.write_videofile(os.path.join(travel_path, filename), verbose=False, logger=None)
+                except:
+                    print(f"ERROR: Failed generating RIFE video")
             # RIFE end
 
         return Processed(p, images if show_images else [], p.seed, initial_info)
